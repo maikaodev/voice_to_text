2291f506b9055c56e2cec630005a79f79d2091f6
maikaodev <maikao.dev@gmail.com>
2021-07-14 16:32:37 -0300

    Adicionando mensagem caso API não exista

--- voice_to_text.js
+++ voice_to_text.js
@@ -11,8 +11,19 @@ if (recognition) {
     button.textContent = listening ? 'Parar de escutar' : 'Aperte para falar'
   })
 
+} else {
+  button.disabled = true
+  // TODO: Melhorar mensagem para leigos
+  textarea.textContent = 'A API SpeechRecognition não está disponível'
 }
 
 function createRecognition() {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    
+  // Caso a API não exista no browser, paramos o programa
+  if (!SpeechRecognition) {
+    return
+  }
+
   return true
 }
