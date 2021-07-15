014118d192fb25bafeac1e07bac77cdaf800827d
maikaodev <82960620+maikaodev@users.noreply.github.com>
2021-07-15 18:04:11 -0300

    Atualizar link ao alterar textarea (#3)
    * Atualizar link ao alterar textarea
    
    * Encodando texto nas URLs de compartilhamento

--- voice_to_text.js
+++ voice_to_text.js
@@ -1,70 +1,76 @@
-const buttonControl = document.querySelector('.btn.control'); //Encadear classe não pode ter espaço.
-const buttonTelegram = document.querySelector('.btn.telegram');
-const buttonWhatsApp = document.querySelector('.btn.whatsapp');
-const textarea = document.querySelector('.text');
-const language = document.getElementById('language');
+const buttonControl = document.querySelector(".btn.control"); //Encadear classe não pode ter espaço.
+const buttonTelegram = document.querySelector(".btn.telegram");
+const buttonWhatsApp = document.querySelector(".btn.whatsapp");
+const textarea = document.querySelector(".text");
+const language = document.getElementById("language");
 
 const recognition = createRecognition();
 
 let listening = false;
 
 if (recognition) {
-  buttonControl.addEventListener('click', () => {
-    if(listening){
+  buttonControl.addEventListener("click", () => {
+    if (listening) {
       recognition.stop();
-    }else{
-      recognition.lang = language.value
+    } else {
+      recognition.lang = language.value;
       recognition.start();
     }
-  })
+  });
+  textarea.addEventListener("input", (evt) => {
+    updateShareButtons(evt.target.value);
+  });
 } else {
-  buttonControl.disabled = true
-  textarea.textContent = 'A API SpeechRecognition não está disponível'
+  buttonControl.disabled = true;
+  textarea.textContent = "A API SpeechRecognition não está disponível";
 }
 
 function createRecognition() {
-  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-    
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
   // Caso a API não exista no browser, paramos o programa
   if (!SpeechRecognition) {
-    return
+    return;
   }
 
   const recognition = new SpeechRecognition();
 
   recognition.onstart = () => {
-    listening = true 
+    listening = true;
     updateButtonText();
-  }
+  };
 
   recognition.onend = () => {
-    listening = false
+    listening = false;
     updateButtonText();
-  }
+  };
 
-  recognition.onerror = err => {
-    console.error(err)
-  }
+  recognition.onerror = (err) => {
+    console.error(err);
+  };
 
-  recognition.onresult = evt => {
-    console.log(evt)
-    textarea.textContent = evt.results[0][0].transcript
+  recognition.onresult = (evt) => {    
+    textarea.textContent = evt.results[0][0].transcript;
     activeShareButtons();
-  }
+    updateShareButtons(textarea.textContent);
+  };
 
-  return recognition
+  return recognition;
 }
 
-function updateButtonText(){
-  buttonControl.textContent = listening ? 'Parar de escutar' : 'Aperte para falar'
+function updateButtonText() {
+  buttonControl.textContent = listening
+    ? "Parar de escutar"
+    : "Aperte para falar";
 }
 
-function activeShareButtons(){
-  buttonTelegram.classList.remove('disabled')
-  buttonWhatsApp.classList.remove('disabled')
+function activeShareButtons() {
+  buttonTelegram.classList.remove("disabled");
+  buttonWhatsApp.classList.remove("disabled");
+}
 
-  const text = textarea.textContent
-
-  buttonTelegram.href = `https://t.me/share/url?text=${text}`
-  buttonWhatsApp.href = `https://api.whatsapp.com/send?text=${text}`
+function updateShareButtons(text) {
+  buttonTelegram.href = `https://t.me/share/url?text=${encodeURIComponent(text)}`;
+  buttonWhatsApp.href = `https://api.whatsapp.com/send?text=${encodeURIComponent(text)}`;
 }
